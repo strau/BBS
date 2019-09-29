078b8753cd0cdf2f3acdf39213afadd0340d2dc0
strau <305139418@qq.com.com>
2019-09-29 19:31:29 +0800

    增加后台用户管理

--- app/Models/User.php
+++ app/Models/User.php
@@ -83,4 +83,28 @@ class User extends Authenticatable implements MustVerifyEmail
         $this->save();
         $this->unreadNotifications->markAsRead();
     }
+
+    public function setPasswordAttribute($password)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($password) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $password = bcrypt($password);
+        }
+
+        $this->attributes['password'] = $password;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! starts_with($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
 }
